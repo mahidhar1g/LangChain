cbf99763f04cf705d9434e19d3651cee3a53a2d8
mahidhar <156211533+mahidhar1g@users.noreply.github.com>
2025-01-08 13:13:31 -0500

    Retreieving

--- rags/1_rag_data_retrive.py
+++ rags/1_rag_data_retrive.py
@@ -2,5 +2,26 @@ import os
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 
-current_dir = os.path.dirname(os.path.ansolute(__file__))
-persistent_directory = os.path.join(current_dir, "db",)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+persistent_directory = os.path.join(current_dir, "db", "chroma_db")
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+db = Chroma(persist_directory=persistent_directory, embedding_function=embeddings)
+
+query = "Where does Gandalf meet Frodo?"
+
+retriever = db.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={
+        "k": 10,
+        "score_threshold": 0.1
+    },
+)
+relevent_docs = retriever.invoke(query)
+
+print("\n--- Relevant Documents ---")
+for i, doc in enumerate(relevent_docs, 1):
+    print(f"Document {i}: {doc.page_content}\n")
+    if doc.metadata:
+        print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")

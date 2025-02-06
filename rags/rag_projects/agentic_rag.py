98da1f265649735de7992470ae223651d7102c93
mahidhar <156211533+mahidhar1g@users.noreply.github.com>
2025-02-05 22:02:48 -0500

    Implemented chunking in the code

--- rags/rag_projects/agentic_rag.py
+++ rags/rag_projects/agentic_rag.py
@@ -6,12 +6,14 @@ from pinecone import Pinecone, ServerlessSpec
 from pinecone.core.openapi.shared.exceptions import PineconeApiException
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 from langchain.schema.runnable import RunnableLambda
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
+
+# Load the PDF file and extract the text
 def load_pdf(folder_path):
     """
     Loads all PDF files from a given folder.
@@ -26,13 +28,12 @@ def load_pdf(folder_path):
     full_text = ""
     try:
         for file_name in os.listdir(folder_path):
-            print("-------------")
-            if file_name.endswith(".pdf"):
+            if file_name.endswith("G.pdf"):
                 file_path = os.path.join(folder_path, file_name)
                 loader = PyPDFLoader(file_path)
                 documents.extend(loader.load())
                 for doc in documents:
-                    full_text += doc.page_content + "\n"
+                    full_text += doc.page_content
         return documents, full_text
     except Exception as e:
         raise RuntimeError(f"An error occurred while reading the PDF file: {e}")
@@ -42,12 +43,47 @@ documents_dir = os.path.join(current_dir, "documents")
 documents, full_text = load_pdf(documents_dir)
 
 
-
-# Split the text into chunks and assign page numbers to each chunk
+# Split the text into chunks and assign metadata to each chunk
 chunk_size = 500
-chunk_overlap = 50
-text_splitter = CharacterTextSplitter(separator="\n", chunk_size=chunk_size, chunk_overlap=chunk_overlap, length_function=len)
+chunk_overlap = 20
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=chunk_size,
+    chunk_overlap=chunk_overlap,
+    separators=["\n\n", "\n", " ", ""],
+    length_function=len
+)
 raw_chunks = text_splitter.split_text(full_text)
 
-print(type(raw_chunks))
-print(len(raw_chunks))
+chunks = []
+for i, doc in enumerate(documents):
+    current_page_text = doc.page_content
+    current_page_metadata = doc.metadata
+    print(current_page_metadata)
+    for chunk in raw_chunks:
+        if chunk in current_page_text or chunk[:50] in current_page_text:
+            if not chunk in current_page_text:
+                chunks.append(
+                    {
+                        "text": chunk.strip(),
+                        "metadata": {
+                            "source":current_page_metadata["source"],
+                            "page":current_page_metadata["page_label"] + "," + str(int(current_page_metadata["page_label"]) + 1)
+                        }
+                    }
+                )
+                break
+            chunks.append(
+                    {
+                        "text": chunk.strip(),
+                        "metadata": {
+                            "source":current_page_metadata["source"],
+                            "page":current_page_metadata["page_label"]
+                        }
+                    }
+                )
+
+
+print(len(chunks))
+for i, chunk in enumerate(chunks):
+    print(f"Chunk {i+1} - {chunk}")
+    print("")
